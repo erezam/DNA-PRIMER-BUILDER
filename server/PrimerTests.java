d74fc35d7a110a4d5117006e945bbc81b0c07204
erezam <amir4246@gmail.com>
2017-11-22 13:45:05 +0200

    gc precent

--- server/PrimerTests.java
+++ server/PrimerTests.java
@@ -1,20 +1,20 @@
-import java.util.Scanner;
 
 public class PrimerTests{
     public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
-            System.out.println("Enter Primer:");
-            String primer;
-            primer = input.next();
-            //System.out.println(GCprecent(primer));
+            String primer = "CCTCTGGAGCGGACTTATTTAC";
+            System.out.println(GCprecent(primer));
             return;
     }
 
     private static int GCprecent(String primer) {   // return G,C precent in primer
-        int count=0;
+        double count=0;
         for(int i=0 ; i<primer.length();i++)
             if((primer.charAt(i)=='G') || (primer.charAt(i)=='C'))
-                count++; 
-        return (count/primer.length())*100;
+                count+=1; 
+        System.out.println(primer.length());        
+        int tmp = (int)((count/(double)primer.length())*100);
+        return tmp;
     }
+
+    
 }
