9b5570c3435c34ce51881e1b455687043ef8fde6
erezam <amir4246@gmail.com>
2017-11-22 13:20:43 +0200

    gc precent func

--- server/PrimerTests.java
+++ server/PrimerTests.java
@@ -1,11 +1,13 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class PrimerTests{
     public static void main(String[] args) {
             Scanner input = new Scanner(System.in);
             System.out.println("Enter Primer:");
-            String primer = input.next();
-            System.out.println(GCprecent(primer));
+            String primer;
+            primer = input.next();
+            //System.out.println(GCprecent(primer));
+            return;
     }
 
     private static int GCprecent(String primer) {   // return G,C precent in primer
