4c9d510b8c8510c1707dd1d9f4092a8b7ecf7df3
orbardugo <orbardugo1@gmail.com>
2018-01-03 16:10:54 +0200

    add export to file

--- PythonServer/Primer_set.py
+++ PythonServer/Primer_set.py
@@ -11,13 +11,16 @@ class Primer_set (object):
     def set_print(self):
         print "Forward id: %s - Reverse id:%s" %(self.forward_primer.id , self.reverse_primer.id)
 
+    def get_amplicon_length(self):
+        return self.reverse_primer.start_index - self.forward_primer.start_index
     def write_to_file(self,file_name):
         write_file = open(file_name, "a")
         write_file.write("----------------------------------------------------------------------------------------------\n" +
                          "FORWARD :\n" +
-                         "Sequence: %s length: %s start index: %s \n"%(self.forward_primer.sequnce, self.forward_primer.length, self.forward_primer.start_index) +
+                         "Sequence: %s length: %s Tm: %s GC: %s start index: %s \n"%(self.forward_primer.sequnce, self.forward_primer.length, self.forward_primer.primer_tm(), self.forward_primer.precent_gc(), self.forward_primer.start_index) +
                          "REVERSE:\n" +
-                         "Sequence: %s length: %s start index: %s \n"%(self.reverse_primer.sequnce, self.reverse_primer.length, self.reverse_primer.start_index) +
-                         "tm difference between F & R: %s \n" % (self.tm_dif()))
+                         "Sequence: %s length: %s Tm: %s GC: %s start index: %s \n"%(self.reverse_primer.sequnce, self.reverse_primer.length,self.reverse_primer.length, self.reverse_primer.primer_tm(), self.reverse_primer.start_index) +
+                         "tm difference between F & R: %s \n" % (self.tm_dif())+
+                         "Amplicon length: %s \n" %(self.get_amplicon_length()))
 
         write_file.close()
